d70ec908a17ac95f7313ca8f95c0e5678ca0ede7
Golovin Pavel <gopavel0@gmail.com>
2018-06-03 18:56:36 +0300

    fix: too many include

--- hw8/main.cpp
+++ hw8/main.cpp
@@ -1,14 +1,3 @@
-#include <signal.h>
-#include <errno.h>
-#include <malloc.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <stdio.h>
-#include <setjmp.h>
-#include <ucontext.h>
-#include <string>
-#include <iostream>
 #include "sigsegv_dump.h"
 
 int main() {
